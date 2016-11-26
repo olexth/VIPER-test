00d109cd9ed6ce2aba3c31803bbfaccf0bc00cfb
Alex Golub <alexeythumb@gmail.com>
2016-11-26 21:32:09 +0200

    Add temporary versions

---
+++ ViperTest/ViperTestTests/MasterPresenterTests.swift
@@ -0,0 +1,35 @@
+//
+//  MasterPresenterTests.swift
+//  ViperTest
+//
+//  Created by Alex Golub on 11/26/16.
+//  Copyright (c) 2016 Alex Golub. All rights reserved.
+//
+
+@testable import ViperTest
+import XCTest
+
+class MasterPresenterTests: XCTestCase {
+    fileprivate var sut: Master!
+    fileprivate var dummyOutput: DummyOutput!
+
+    override func setUp() {
+        super.setUp()
+        sut = Master()
+        dummyOutput = DummyOutput()
+        sut.output = dummyOutput
+    }
+
+    override func tearDown() {
+        dummyOutput = nil
+        sut = nil
+        super.tearDown()
+    }
+}
+
+fileprivate class DummyOutput: MasterOutput {
+    var didInvokeFunction = false
+    func function() {
+        didInvokeFunction = true
+    }
+}
